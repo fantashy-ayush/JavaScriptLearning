5a45e6229b2ef039ef7e016043cd498c9819bf59
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-20 10:31:34 +0000

    new file created

---
+++ 01_basic/datatype.js
@@ -0,0 +1,10 @@
+let score = "33abs"
+console.log(typeof score);
+let valueinnumber=Number(score);
+console.log(typeof valueinnumber);
+console.log(valueinnumber)
+
+let bool=""
+let booleanVariable=Boolean(bool)
+console.log(booleanVariable)
+// ""=> false
