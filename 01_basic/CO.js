974b1b9c0ca938795b89caca1abbd10e3ef4d493
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-20 13:08:42 +0000

    added the comparison

--- 01_basic/CO.js
+++ 01_basic/CO.js
@@ -3,8 +3,12 @@
 //let str3 = str1 + str2; // str3 becomes "helloAyush"
 //console.log(str3);      // prints: helloAyush
 //console.log(3+6*5/55%6)
-console.log(+true)
-console.log(+"")
-let num1,num2,num3;
-num1=num2=num3=4
-console.log(num1)
+//console.log(+true)
+//console.log(+"")
+//let num1,num2,num3;
+//num1=num2=num3=4
+//console.log(num1)
+
+console.log(null>0)
+console.log(null>=0)
+console.log(null===0)
