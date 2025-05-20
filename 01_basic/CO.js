4d01042f042f0404b1ec186063ca7af5190f2f70
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-20 13:01:08 +0000

    new file

---
+++ 01_basic/CO.js
@@ -0,0 +1,10 @@
+//let str1 = "hello";     // str1 contains "hello"
+//let str2 = "Ayush";     // str2 contains "Ayush"
+//let str3 = str1 + str2; // str3 becomes "helloAyush"
+//console.log(str3);      // prints: helloAyush
+//console.log(3+6*5/55%6)
+console.log(+true)
+console.log(+"")
+let num1,num2,num3;
+num1=num2=num3=4
+console.log(num1)
