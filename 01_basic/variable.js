7f3b11b24fdc70e33c1eb9422b715bec1d9f1d7d
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-20 09:47:29 +0000

    new file

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -1,7 +1,15 @@
-const accountID=144549
-let accountEmail="ayushkumarsharma102@gmail.com"
-var accountpassword="12345"
-var accountCity="Jaipur"
-accountID=2
+const accountID = 144549;
+let accountEmail = "ayushkumarsharma102@gmail.com";
+var accountpassword = "12345";
+let accountcity = "Jaipur";
 
-console.log(accountID)
+// accountID = 2; // This will throw an error since accountID is a const
+
+console.log(accountID);
+
+console.table({
+  accountID,
+  accountEmail,
+  accountpassword,
+  accountcity
+});
