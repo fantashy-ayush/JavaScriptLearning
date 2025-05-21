335855674c3f738a43b55c1ae31f6a70fcd435c7
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-21 11:22:37 +0000

    fnew file created date

---
+++ 01_basic/08Dates.js
@@ -0,0 +1,22 @@
+let myDate= new Date()
+/*
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(typeof myDate)
+
+let myCreatedDate=new Date(2023, 0, 23)
+console.log(myCreatedDate.toDateString())
+let myCreatedDate=new Date("2023-01-14")
+console.log(myCreatedDate.toLocaleString())
+*/
+let myCreatedDate=new Date("01-14-2023")
+let mytimestamp=Date.now()
+//console.log(mytimestamp)
+//console.log(myCreatedDate.getTime())
+//console.log(Math.floor(Date.now()/1000));
+let newdate=new Date()
+console.log(newdate)
+console.log(newdate.getMonth()+1)
+console.log(newdate.getDay())
