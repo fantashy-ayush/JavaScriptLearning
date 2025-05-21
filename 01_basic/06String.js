a18c78f0873c77870d42b92abac2401231ca33c0
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-21 09:53:36 +0000

    new file created

---
+++ 01_basic/06String.js
@@ -0,0 +1,16 @@
+const name="Ayush"
+const repo=5
+
+console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+const gamename=new String('AyushAKS')
+
+console.log(gamename[0])
+console.log(gamename.length);
+console.log(gamename.toUpperCase())
+
+const newstr=gamename.substring(3,6)
+console.log(newstr)
+
+const newstr1=gamename.slice(-7,3)
+console.log(newstr1)
