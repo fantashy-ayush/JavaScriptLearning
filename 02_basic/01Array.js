03c7a3b45700283d7d793816e4ea7037ce420316
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-21 13:17:16 +0000

    Array file and folder

---
+++ 02_basic/01Array.js
@@ -0,0 +1,25 @@
+//Array
+const Array =[1,2,3,4,5]
+console.log(Array[0])
+Array.unshift(9)
+console.log(Array)
+
+// Array method
+/*
+Array.push(6)
+Array.push(7)
+Array.pop()
+console.log(Array)
+
+console.log(Array.includes(9))
+console.log(Array.indexOf(5))
+*/
+
+const myn1 =Array.slice(1,3)
+
+console.log(myn1);
+console.log("B",Array);
+
+const myn2=Array.splice(1,3)
+console.log(myn2);
+console.log(Array)
