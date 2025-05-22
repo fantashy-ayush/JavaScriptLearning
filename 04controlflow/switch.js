8bd7bc298aed7d880efad98ad82467247572c4c2
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-22 13:40:19 +0000

    new file created

---
+++ 04controlflow/switch.js
@@ -0,0 +1,53 @@
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+const month=3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("october");
+        break;
+    case 11:
+        console.log("november");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("no case available");
+        break;
+}
