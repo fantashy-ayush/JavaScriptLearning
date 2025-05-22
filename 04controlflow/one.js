a2b240bfee9f7f06f0dee056d47e7e37cf931b94
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-22 12:34:24 +0000

    new file created

---
+++ 04controlflow/one.js
@@ -0,0 +1,46 @@
+// if
+
+//const isloggedIn=true
+const temp=42
+/*
+if(temp ===50){
+    console.log("exe");
+}else{
+    console.log("not exe");
+}
+const score=200
+
+if(score>100){
+    let power="fly"
+    console.log(`user power : ${power}`);
+    
+}
+console.log(`user power : ${power}`);
+*/
+
+
+const balance=1000
+//if(balance>500) console.log("withdraw"), console.log("test");\
+
+/*
+if (balance<500){
+    console.log("less than 500");   
+}else if (balance<900) {
+    console.log("less than 750");
+} else {
+    console.log("less than 1200");
+    
+}
+*/
+const isloggedIn=true
+const debitcard=true
+const loggedgoogle=false
+const loggedEmail=true
+
+if(isloggedIn && debitcard && 2==3){
+    console.log("allow to purchase");
+}
+if (loggedgoogle || loggedEmail ) {
+    console.log("User logged in");
+    
+}
