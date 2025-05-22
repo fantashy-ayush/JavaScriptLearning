d35a8309c842f84caf45dda4920ab32753a2a840
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-22 21:14:11 +0000

    new file

---
+++ 05Iteration/05.js
@@ -0,0 +1,40 @@
+const prog=['js','ruby','pyton','java','cpp']
+
+//prog.forEach( function(item) {
+//    console.log(item);
+//    
+//} )
+
+//prog.forEach((item)=>{
+//    console.log(item);
+//    
+//})
+
+//function printMe(item){
+//    console.log(item);
+//    
+//}
+//prog.forEach(printMe)
+
+
+//prog.forEach((item, index, arr) => {
+//    console.log(item, index, arr);
+//})
+
+
+const mycoding =[
+    {
+    languageName:'javascript',
+    languageFileName:'js'
+    },
+    {
+
+    },
+    {
+
+    }
+]
+mycoding.forEach((item)=>{
+    console.log(item.languageName);
+    
+})
