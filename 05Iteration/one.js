bbf9aa9a6264feb66c4fd2ddc42576dd01da55cc
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-22 14:38:13 +0000

    new file created

---
+++ 05Iteration/one.js
@@ -0,0 +1,17 @@
+// for loop
+
+for (let index = 0; index <10; index++) {
+const element = index;
+    if (index===5) {
+      console.log("Ayush");
+      
+    }
+    console.log(element);  
+ }
+ console.log();
+let myarray=["Superman", "batman", "flash"]
+
+for (let index = 0; index < array.length; index++) {
+   const element = array[index];
+   console.log();
+}
