7c67953208fb71928379e1b7a0472bb5d8944c4f
fantashy-ayush <129979382+fantashy-ayush@users.noreply.github.com>
2025-05-22 20:50:21 +0000

    new file

---
+++ 05Iteration/04.js
@@ -0,0 +1,31 @@
+const myobject={
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myobject) {
+    //console.log(`${key} shortcut is for ${myobject[key]}`);
+    
+}
+
+const prog=['js','rb','py','java','cpp']
+
+for (const key in prog) {
+    //console.log(prog[key]);
+    
+}
+
+
+//const map = new Map()
+//map.set('IN', "India")
+//map.set('USA', "United States of America")
+//map.set('FR', "France")
+//map.set('BG', "Bangladesh")
+//map.set('BG', "Bangladesh")
+//
+//for (const key in map) {
+//    console.log(key);
+//    
+//}
